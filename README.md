a6464530fbf5444be79473ed84a343e611599bc5
OwoeyeMoyosore <moyosoreowoeye2000@gmail.com>
2023-06-20 10:05:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Stackblitz-Camera-Capture
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-ia9obm)
---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useCallback, useRef, useState } from 'react';
+import './style.css';
+import Webcam from 'react-webcam';
+import React = require('react');
+
+const CustomWebcam = () => {
+  const webcamRef = useRef(null);
+  const [imgSrc, setImgSrc] = useState(null);
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc(imageSrc);
+  }, [webcamRef]);
+
+  const retake = () => {
+    setImgSrc(null);
+  };
+  return (
+    <div className="container">
+      {imgSrc ? (
+        <img src={imgSrc} alt="webcam" />
+      ) : (
+        <Webcam height={600} width={600} ref={webcamRef} />
+      )}
+      <div className="btn-container">
+      {imgSrc ? (
+          <button onClick={retake}>Retake photo</button>
+        ) : (
+          <button onClick={capture}>Capture photo</button>
+        )}
+      </div>
+      {/* <Webcam height={600} width={600} ref={webcamRef} /> */}
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <CustomWebcam />
+    </div>
+  );
+}
